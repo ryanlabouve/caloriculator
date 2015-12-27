44e18cebccc8e0141f344ecea1e03c26cac070a6
Ryan LaBouve <imryanlabouve@gmail.com>
2015-12-27 11:36:47 -0600

    Finish iifym box

--- app/models/diet.js
+++ app/models/diet.js
@@ -1,8 +1,9 @@
 import DS from 'ember-data';
 import EmberCPM from 'ember-cpm';
 
+const { computed } = Ember;
 const { computed: {equal}} = Ember;
-const { Macros: { sum, difference, product, conditional }} = EmberCPM;
+const { Macros: { sum, difference, product, quotient, conditional }} = EmberCPM;
 
 export default DS.Model.extend({
   _proteinCalories: 4,
@@ -10,6 +11,7 @@ export default DS.Model.extend({
   _fatCalories: 9,
 
   human: null,
+
 
   bmr: conditional(
     equal('human.sex', 'male'),
@@ -35,4 +37,57 @@ export default DS.Model.extend({
   bmrLightActivity: product('bmr', 1.375),
   bmrModerateActivity: product('bmr', 1.55),
   bmrIntenseActivity: product('bmr', 1.725),
+
+  proteinPerPound: 1.1,
+  carbsPerPoundNoActivity: 0.4,
+  carbsPerPoundLightActivity: 1.0,
+  carbsPerPoundModerateActivity: 1.3,
+  carbsPerPoundIntenseActivity: 1.7,
+
+  proteinNoActivity: product('human.weight', 'proteinPerPound'),
+  proteinLightActivity: computed.alias('proteinNoActivity'),
+  proteinModerateActivity: computed.alias('proteinNoActivity'),
+  proteinIntenseActivity: computed.alias('proteinNoActivity'),
+
+
+  carbsNoActivity: product('human.weight', 'carbsPerPoundNoActivity'),
+  carbsLightActivity: product('human.weight', 'carbsPerPoundLightActivity'),
+  carbsModerateActivity: product('human.weight', 'carbsPerPoundModerateActivity'),
+  carbsIntenseActivity: product('human.weight', 'carbsPerPoundIntenseActivity'),
+
+  _caloriesNoFatsNoActivity: difference(
+    'bmrNoActivity',
+    sum(
+      product('carbsNoActivity', '_carbCalories'),
+      product('proteinNoActivity', '_proteinCalories')
+    )
+  ),
+  fatsNoActivity: quotient('_caloriesNoFatsNoActivity', '_fatCalories'),
+
+  _caloriesNoFatsLightActivity: difference(
+    'bmrLightActivity',
+    sum(
+      product('carbsLightActivity', '_carbCalories'),
+      product('proteinLightActivity', '_proteinCalories')
+    )
+  ),
+  fatsLightActivity: quotient('_caloriesNoFatsLightActivity', '_fatCalories'),
+
+  _caloriesNoFatsModerateActivity: difference(
+    'bmrModerateActivity',
+    sum(
+      product('carbsModerateActivity', '_carbCalories'),
+      product('proteinModerateActivity', '_proteinCalories')
+    )
+  ),
+  fatsModerateActivity: quotient('_caloriesNoFatsModerateActivity', '_fatCalories'),
+
+  _caloriesNoFatsIntenseActivity: difference(
+    'bmrIntenseActivity',
+    sum(
+      product('carbsIntenseActivity', '_carbCalories'),
+      product('proteinIntenseActivity', '_proteinCalories')
+    )
+  ),
+  fatsIntenseActivity: quotient('_caloriesNoFatsIntenseActivity', '_fatCalories'),
 });
