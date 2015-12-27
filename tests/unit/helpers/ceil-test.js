0915b6719ae7f4cf85345ccef7bae3e81701c9ab
Ryan LaBouve <imryanlabouve@gmail.com>
2015-12-27 10:48:33 -0600

    Polish first two modules

---
+++ tests/unit/helpers/ceil-test.js
@@ -0,0 +1,10 @@
+import { ceil } from '../../../helpers/ceil';
+import { module, test } from 'qunit';
+
+module('Unit | Helper | ceil');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  let result = ceil(42);
+  assert.ok(result);
+});
