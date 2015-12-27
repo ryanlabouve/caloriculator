b68a47347fd3724401d6bbc5134833d0e1b74f0c
Ryan LaBouve <imryanlabouve@gmail.com>
2015-12-27 14:29:54 -0600

    Unit labels and human readable height on stats

--- app/components/cl-stats/component.js
+++ app/components/cl-stats/component.js
@@ -1,4 +1,9 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  humanReadableHeight: Ember.computed('human.height', function() {
+    const feet = Math.floor(this.get('human.height') / 12);
+    const inches = this.get('human.height') % 12;
+    return `${feet}' ${inches}''`;
+  })
 });
