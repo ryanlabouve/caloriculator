0c3d024de07ac9b510df93140bfc71d2c6a0eef2
Ryan LaBouve <imryanlabouve@gmail.com>
2015-12-27 15:07:26 -0600

    Show differences between bmr and adjusted on iifym

--- app/components/cl-iifym/component.js
+++ app/components/cl-iifym/component.js
@@ -1,9 +1,17 @@
 import Ember from 'ember';
+import EmberCPM from 'ember-cpm';
+
+const { Macros: { sum, difference, product }} = EmberCPM;
 
 export default Ember.Component.extend({
   didReceiveAttrs() {
     const diet = this.get('diet');
     const human = this.get('human');
     diet.set('human', human);
-  }
+  },
+
+  diffNoActivity: difference('diet.adjustedBmrNoActivity', 'diet.bmrNoActivity'),
+  diffLightActivity: difference('diet.adjustedBmrLightActivity', 'diet.bmrLightActivity'),
+  diffModerateActivity: difference('diet.adjustedBmrModerateActivity', 'diet.bmrModerateActivity'),
+  diffIntenseActivity: difference('diet.adjustedBmrIntenseActivity', 'diet.bmrIntenseActivity'),
 });
---
+++ tests/unit/helpers/difference-test.js
@@ -0,0 +1,10 @@
+import { difference } from '../../../helpers/difference';
+import { module, test } from 'qunit';
+
+module('Unit | Helper | difference');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  let result = difference(42);
+  assert.ok(result);
+});
