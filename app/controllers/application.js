932a41e6d19296d614ac08212ac2d807141884f1
Ryan LaBouve <imryanlabouve@gmail.com>
2017-07-09 17:05:01 -0500

    Start hacking with QPs

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,4 +1,21 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  queryParams: [
+    'human[weight]',
+    'human[name]',
+    'human[height]',
+    'human[age]',
+    'human[sex]'
+  ],
+  human: null,
+  diet: null,
+  asdf: Ember.observer('model.human.{name,weight,height,age,sex}', function() {
+    Ember.run.debounce(null, () => {
+      let humanProperties = ['name', 'weight', 'height', 'age', 'sex'];
+      humanProperties.forEach((prop) => {
+        this.set(`human[${prop}]`, this.get(`model.human.${prop}`));
+      }, 150);
+    });
+  })
 });
