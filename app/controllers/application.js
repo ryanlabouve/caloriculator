0efba24884453dc23bd1f27947cbd3d294ca473f
Ryan LaBouve <imryanlabouve@gmail.com>
2017-07-09 22:21:02 -0500

    Routable by everything and publish via surge

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,4 +1,5 @@
 import Ember from 'ember';
+import { task, timeout } from 'ember-concurrency';
 
 export default Ember.Controller.extend({
   queryParams: [
@@ -6,16 +7,34 @@ export default Ember.Controller.extend({
     'human[name]',
     'human[height]',
     'human[age]',
-    'human[sex]'
+    'human[sex]',
+    'diet[calorieAdjustment]',
+    'diet[proteinPerPound]',
+    'diet[carbsPerPoundNoActivity]',
+    'diet[carbsPerPoundLightActivity]',
+    'diet[carbsPerPoundModerateActivity]',
+    'diet[carbsPerPoundIntenseActivity]'
   ],
   human: null,
   diet: null,
 
-  asdf: Ember.observer('model.human.{name,weight,height,age,sex}', function() {
-    // Debounce
+  asdf: Ember.observer(
+    'model.human.{name,weight,height,age,sex}',
+    'model.diet.{calorieAdjustment,proteinPerPound,carbsPerPoundNoActivity,carbsPerPoundLightActivity,carbsPerPoundModerateActivity,carbsPerPoundIntenseActivity}',
+    function() {
+    this.get('syncQPs').perform();
+  }),
+
+  syncQPs: task(function*() {
+    yield timeout(100);
     let humanProperties = ['name', 'weight', 'height', 'age', 'sex'];
     humanProperties.forEach((prop) => {
       this.set(`human[${prop}]`, this.get(`model.human.${prop}`));
     });
-  })
+
+    let dietProperties = ['calorieAdjustment', 'proteinPerPound', 'carbsPerPoundNoActivity', 'carbsPerPoundLightActivity', 'carbsPerPoundModerateActivity', 'carbsPerPoundIntenseActivity'];
+    dietProperties.forEach((prop) => {
+      this.set(`diet[${prop}]`, this.get(`model.diet.${prop}`));
+    });
+  }).restartable()
 });
